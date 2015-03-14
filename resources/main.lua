d02415c0e067605cca81d59d6a12d73300fb0810
aarguelles <aarguelles@uninorte.edu.co>
2015-03-14 11:31:16 -0500

    Structuration of the app
    Divided the app in several files (structuration)
    Trial splashart

--- resources/main.lua
+++ resources/main.lua
@@ -1,2 +1,8 @@
--- Your app starts here!
-local label= director:createLabel(director.displayCenterX, director.displayCenterY, 'Habla cole!')
+-- Developed by ByteArt
+-- This is the main .lua file. It is the root file to the other parts of the app
+require("game1")
+require("pauseMenu")
+require("mainMenu")
+
+
+-- director:moveToScene(menuScene,{transitionType="slideInL", transitionTime=0.5})
---
+++ resources/mainMenu.lua
@@ -0,0 +1,8 @@
+-- Developed by ByteArt
+-- This is the main Menu
+
+menuScene = director:createScene()
+
+local label= director:createLabel(director.displayCenterX, (director.displayHeight - 20), 'mainMenu')
+print(director.displayHeight - label.yText)
+
